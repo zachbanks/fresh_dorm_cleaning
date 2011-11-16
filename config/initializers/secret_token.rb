f66c8db47b5d02c45c54ea8ad6a6c040088371ab
Zach Banks <zachbanks@me.com>
2011-11-16 17:58:52 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreshDormCleaning::Application.config.secret_token = '55ad0de283707f55ec570db092ac2630c31ee90ec6bb0b218c0e21c0454b55db4d307c59ebbe088754e9c3c8000dc1cd9d5932832910baec1418e8724e135033'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FreshDormCleaning::Application.config.session_store :cookie_store, key: '_fresh_dorm_cleaning_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FreshDormCleaning::Application.config.session_store :active_record_store
