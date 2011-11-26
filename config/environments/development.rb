2eec4bfb3a7f575efddc5cfaeab9b121de26345e
Zach Banks <zachbanks@me.com>
2011-11-25 23:30:43 -0500

    Added mailer configuration code to development.rb and production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,3 +34,16 @@ silence_warnings do
   require 'pry'
   IRB = Pry
 end
+
+# Feedback Mailer.
+config.action_mailer.default_url_options = { :host => "localhost:3000" }
+
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "gmail.com",
+  :user_name            => "cityboyzoutdoors@gmail.com",
+  :password             => "codyisqueer",
+  :authentication       => :plain,
+  :enable_starttls_auto => true
+}
