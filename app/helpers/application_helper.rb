80763dbe03dfec6c74b71c68b2d65fd0625329b6
Zach Banks <zachbanks@me.com>
2011-11-29 05:43:20 -0500

    Wrote a format_phone_number view helper method which formats a raw phone number string into a nice phone number. It is currently placed in ApplicationHelper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,26 @@ module ApplicationHelper
     base_title = "Fresh Dorm Cleaning"
     @title ? "#{base_title} | #{@title}" : base_title
   end
+  
+  # Takes a phone number in the format of 1234567890 and returns it in a specified format. Returns nil if bad format is given.
+  # Supported formats:
+  # :dashed (default) - 123-456-7890
+  # :dotted - 123.456.7890
+  # :parenthesis - (123) 456-7890
+  def format_phone_number(phone_number, format=:dashed)
+    supported_formats = [:dashed, :dotted, :parenthesis]
+    return nil unless supported_formats.include?(format)
+    
+    formatted_number = ''
+    number = phone_number.to_i
+    
+    case format
+    when :dashed
+      formatted_number = number_to_phone(number)
+    when :dotted
+      formatted_number = number_to_phone(number, :delimiter => '.')
+    when :parenthesis
+      formatted_number = number_to_phone(number, :area_code => true)
+    end
+  end
 end
