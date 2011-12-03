1193fb4baf20030d00b5d0ea3f90808290b3bcb1
Zach Banks <zachbanks@me.com>
2011-12-03 04:14:47 -0500

    Updated appoinment_spec.rb.

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,17 +1,26 @@
 require 'spec_helper'
 
 describe Appointment do
+  subject { Appointment.new }
+  
   describe "basic functions" do
-    it "should have an appointment date"
-    it "should have access to the person's contact information"
+    it "should have an appointment date" do
+      subject.date = DateTime.now
+      subject.date.should_not be_nil
+    end
+    
+    it "should have access to the client's contact information" do
+      client = Factory(:client)
+      subject.client = client
+      subject.client.should_not be_nil
+    end
   end
   
   describe "validations" do
-    it "should validate presence of the appointment date"
+    it { should validate_presence_of(:date) }
   end
   
   describe "associations" do
     it { should belong_to(:client) }
-    it { should validate_presence_of(:date) }
   end
 end
